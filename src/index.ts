240639f47d528cde6c77f13a22b58b6e350163e2
Reece Dunham <me@rdil.rocks>
2021-11-15 16:07:43 -0500

    It does work. Add more node types.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
+import { createHash } from "crypto"
+
 export type Globals = Record<string, string | boolean | number>
 
+export const _globalsCache: Map<string, Record<string, unknown>> = new Map()
+
 abstract class INode {
-    protected constructor(data: unknown[], globals: Globals) {
+    protected constructor(data: unknown | unknown[], globalsHash: string) {
         this.data = data
-        this.globals = globals
+        this.globalsHash = globalsHash
+        this.serialize()
     }
 
     isNode = true
 
-    data: unknown[]
-    globals: Globals
+    data: unknown | unknown[]
+    globalsHash: string
 
-    serialize() {
+    protected serialize() {
         let i = 0
 
         while (this.data && this.data[i]) {
             let item = this.data[i]
 
             if (typeof item === "string" && item.includes(".")) {
-                item = $referenceToData(item, this.globals)
+                item = $referenceToData(item, this.globalsHash)
             }
 
             if (typeof item === "object") {
-                item = getNewNodes(item, this.globals)
+                item = getNewNodes(item, this.globalsHash)
             }
 
             this.data[i] = item
@@ -30,12 +35,12 @@ abstract class INode {
         }
     }
 
-    abstract solve(): boolean
+    abstract solve(): boolean | number
 }
 
-function $referenceToData(reference: unknown, globals: Globals) {
+function $referenceToData(reference: unknown, globalsHash: string) {
     const clone = `${reference}`
-    reference = globals
+    reference = _globalsCache.get(globalsHash)
     // the thing has a dot in it, which means that its accessing a global
     const parts = clone.split(".")
 
@@ -48,14 +53,11 @@ function $referenceToData(reference: unknown, globals: Globals) {
 }
 
 class EqNode extends INode {
-    data: unknown[]
-
-    public constructor(data: unknown[], globals: Globals) {
-        super(data, globals)
-        this.serialize()
+    public constructor(data: unknown | unknown[], globalsHash: string) {
+        super(data, globalsHash)
     }
 
-    solve(): boolean {
+    override solve(): boolean {
         let item1 = this.data[0]
         let item2 = this.data[1]
 
@@ -72,19 +74,18 @@ class EqNode extends INode {
 }
 
 class AndNode extends INode {
-    public constructor(data: unknown[], globals: Globals) {
-        super(data, globals)
-        this.serialize()
+    public constructor(data: unknown | unknown[], globalsHash: string) {
+        super(data, globalsHash)
     }
 
-    solve(): boolean {
+    override solve(): boolean {
         let i = 0
 
         while (this.data && this.data[i]) {
             let item = this.data[i]
 
             if (item.isNode) {
-                item = item.solve()
+                item = item.solve() as boolean
             }
 
             if (!item) {
@@ -99,20 +100,52 @@ class AndNode extends INode {
 }
 
 class NotNode extends INode {
-    public constructor(data: unknown[], globals: Globals) {
-        super(data, globals)
-        this.serialize()
+    public constructor(data: unknown | unknown[], globalsHash: string) {
+        super(data, globalsHash)
     }
 
-    solve(): boolean {
+    override solve(): boolean {
         let item = this.data as INode
         let bool = false
 
         if ((item as INode).isNode) {
-            bool = item.solve()
+            bool = item.solve() as boolean
         }
 
         return !bool
+    }
+}
+
+class PushUniqueNode extends INode {
+    public constructor(data: unknown | unknown[], globalsHash: string) {
+        super(data, globalsHash)
+    }
+
+    override solve(): boolean {
+        let item1 = this.data[0]
+        let item2 = this.data[1] as unknown[]
+
+        const currentLength = item2.length
+
+        return currentLength !== item2.push(item1)
+    }
+}
+
+class MulNode extends INode {
+    public constructor(data: unknown | unknown[], globalsHash: string) {
+        super(data, globalsHash)
+    }
+
+    override solve(): number {
+        let num = 1
+        let i = 0
+
+        while (this.data && this.data[i]) {
+            num *= this.data[i] as number
+            i++
+        }
+
+        return num
     }
 }
 
@@ -120,29 +153,47 @@ function hasOwn(target: unknown, value: string): boolean {
     return Object.prototype.hasOwnProperty.call(target, value)
 }
 
-function getNewNodes(parent: unknown, globals: Globals): undefined | INode {
+function getNewNodes(parent: unknown, globalsHash: string): undefined | INode {
     if (hasOwn(parent, "$eq")) {
-        return new EqNode(parent.$eq, globals)
+        return new EqNode((parent as { $eq: unknown })["$eq"], globalsHash)
     }
 
     if (hasOwn(parent, "$and")) {
-        return new AndNode(parent.$and, globals)
+        return new AndNode(parent["$and"], globalsHash)
     }
 
     if (hasOwn(parent, "$not")) {
-        return new NotNode(parent.$not, globals)
+        return new NotNode(parent["$not"], globalsHash)
+    }
+
+    if (hasOwn(parent, "$pushunique")) {
+        return new PushUniqueNode(parent["$pushunique"], globalsHash)
+    }
+
+    if (hasOwn(parent, "$mul")) {
+        return new MulNode(parent["$mul"], globalsHash)
     }
 }
 
-export class StateMachine {
-    globals: Globals
+function calculateGlobalsHash(globals: Globals): string {
+    const hash = createHash("sha256")
 
-    constructor(globals: Globals) {
-        this.globals = globals
+    hash.update(JSON.stringify(globals))
+
+    return hash.digest("hex")
+}
+
+export class StateMachine {
+    protected _globalsHash: string
+
+    public constructor(globals: Globals) {
+        const globalsHash = (this._globalsHash = calculateGlobalsHash(globals))
+
+        _globalsCache.set(globalsHash, globals)
     }
 
-    check(params: unknown): boolean {
-        const n = getNewNodes(params, this.globals)
-        return n.solve() // should be a boolean
+    public check(params: unknown): boolean {
+        const n = getNewNodes(params, this._globalsHash)
+        return n.solve() as boolean // should be a boolean
     }
 }
