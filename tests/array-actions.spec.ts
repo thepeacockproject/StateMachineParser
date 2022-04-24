b4ff08725b942aab4abfb324a8d4355696ae83de
Reece Dunham <me@rdil.rocks>
2022-04-24 16:35:25 -0400

    Finish the array-actions test

--- tests/array-actions.spec.ts
+++ tests/array-actions.spec.ts
@@ -9,11 +9,27 @@ const data = {
         {
             Animals: ["dog", "cat", "bird"]
         },
+    ],
+    PushUnique1: [
+        {
+            $pushunique: ["$People", "Viktor Novikov"]
+        },
+        {
+            People: ["Dalia Margolis", "Silvio Caruso", "Francesca De Santis"]
+        },
+    ],
+    PushUnique2: [
+        {
+            $pushunique: ["$People", "Dalia Margolis"]
+        },
+        {
+            People: ["Viktor Novikov", "Dalia Margolis", "Silvio Caruso", "Francesca De Santis"]
+        },
     ]
 }
 
 describe("$push", () => {
-    it("can push an element and cause a different", () => {
+    it("can push an element and cause a different output", () => {
         const [sm, vars] = data.Push1
         assert.strictEqual(vars.Animals.length, 3)
 
@@ -25,5 +41,24 @@ describe("$push", () => {
 })
 
 describe("$pushunique", () => {
+    it("can push just like $push", () => {
+        const [sm, vars] = data.PushUnique1
+        assert.strictEqual(vars.People.length, 3)
 
+        const output = handleActions(sm, vars)
+
+        assert.strictEqual(output.People.length, 4)
+        assert.strictEqual(output.People[3], "Viktor Novikov")
+    })
+
+    it("obeys the unique part of its job", () => {
+        const [sm, vars] = data.PushUnique2
+        assert.strictEqual(vars.People.length, 4)
+
+        const output = handleActions(sm, vars)
+
+        assert.strictEqual(output.People.length, 4)
+        // check the last item in it, which should still be Franny
+        assert.strictEqual(output.People.reverse()[0], "Francesca De Santis")
+    })
 })
