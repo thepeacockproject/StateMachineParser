81d324f9c97c51a3621f594da14ec9c623589f96
Reece Dunham <me@rdil.rocks>
2022-05-27 23:30:26 -0400

    Add sha1 tests (raises code coverage)

--- tests/misc.spec.ts
+++ tests/misc.spec.ts
@@ -1,5 +1,7 @@
-import { set } from "../src/utils"
+import { set, sha1 } from "../src/utils"
 import assert from "assert"
+
+// the main purpose of this is just to improve code coverage
 
 describe("utils", () => {
     describe("dset", () => {
@@ -13,5 +15,12 @@ describe("utils", () => {
             // @ts-expect-error I know.
             assert.strictEqual(Object.prototype.func, undefined, "prototype pollution succeeded")
         })
+    })
+
+    describe("sha1", () => {
+        assert.strictEqual(sha1(""), "da39a3ee5e6b4b0d3255bfef95601890afd80709", "sha1 calc of empty string is wrong")
+        assert.strictEqual(sha1("hello-world"), "fbb969117edfa916b86dfb67fd11decf1e336df0", "sha1 calc of 'hello-world' is wrong")
+        assert.strictEqual(sha1("abcdef"), "1f8ac10f23c5b5bc1167bda84b833e5c057a77d2", "sha1 calc of 'abcdef' is wrong")
+        assert.strictEqual(sha1("128734598"), "6c7ffcb2e7e8bef9e956b7998c48cf4d9d53d831", "sha1 calc of '128734598' is wrong")
     })
 })
